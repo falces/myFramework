258eba55024bda19f5cb5bc87272f07953ccbf4e
Javier Rodríguez <javier.rodriguez@digimobil.es>
2021-11-30 10:54:03 +0100

    ADD: Manage exceptions

--- public/index.php
+++ public/index.php
@@ -4,9 +4,13 @@ require dirname(__DIR__).'/vendor/autoload.php';
 use App\Controllers\User;
 use Falces\DotEnvLoader;
 
-$environmentVariables = new DotEnvLoader(dirname(__DIR__).'/.env');
-$environmentVariables->load();
-var_dump($environmentVariables->getEnvironmentVariables());
+try{
+    $environmentVariables = new DotEnvLoader(dirname(__DIR__));
+    $environmentVariables->load();
+    var_dump($environmentVariables->getEnvironmentVariables());
+} catch (InvalidArgumentException $e){
+    echo 'Error: ' . $e->getMessage() . ' (' . $e->getCode() . ')';
+}
 
 $user = new User('Javi');
 echo $user->getName() . PHP_EOL;
